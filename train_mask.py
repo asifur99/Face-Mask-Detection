94771136d01b6954f60a3d3223a3d2a58f4555ea
Asifur Rahman <asifur2014@hotmail.com>
2022-03-26 16:46:21 -0400

    Adding code for plotting the results in graph

--- train_mask.py
+++ train_mask.py
@@ -1,23 +1,25 @@
-import numpy as np
 import os
+# Disabling GPU registration
+os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
 
-from tensorflow.keras.applications.mobilenet_v2 import preprocess_input
-from tensorflow.keras.preprocessing.image import img_to_array
-from tensorflow.keras.preprocessing.image import load_img
-from tensorflow.keras.utils import to_categorical
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
 from tensorflow.keras.applications import MobileNetV2
-from tensorflow.keras.layers import Input
 from tensorflow.keras.layers import AveragePooling2D
 from tensorflow.keras.layers import Dropout
 from tensorflow.keras.layers import Flatten
 from tensorflow.keras.layers import Dense
+from tensorflow.keras.layers import Input
 from tensorflow.keras.models import Model
 from tensorflow.keras.optimizers import Adam
-
+from tensorflow.keras.applications.mobilenet_v2 import preprocess_input
+from tensorflow.keras.preprocessing.image import img_to_array
+from tensorflow.keras.preprocessing.image import load_img
+from tensorflow.keras.utils import to_categorical
 from sklearn.preprocessing import LabelBinarizer
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import classification_report
+import matplotlib.pyplot as plt
+import numpy as np
 
 DIRECTORY = r"C:\Users\asifu\Desktop\GitHub\Face-Mask-Detection\dataset"
 CATEGORIES = ["with_mask", "without_mask"]
@@ -41,15 +43,15 @@ for category in CATEGORIES:
         data.append(image)
         labels.append(category)
 
+data = np.array(data, dtype="float32")
+labels = np.array(labels)
+
 # converting categorical variables as binary vectors (one-hot encoding)
 lb = LabelBinarizer()
 labels = lb.fit_transform(labels)
 labels = to_categorical(labels)
 
-data = np.array(data, dtype="float32")
-labels = np.array(labels)
-
-(trainX, testX, trainY, testY) = train_test_split(data, labels,	test_size=0.22, stratify=labels, random_state=42)
+(trainX, testX, trainY, testY) = train_test_split(data, labels,	test_size=0.20, stratify=labels, random_state=42)
 
 # construct the training image generator for data augmentation
 augImg = ImageDataGenerator(
@@ -82,17 +84,17 @@ for layer in baseModel.layers:
 
 # initialize the initial learning rate, number of epochs to train for and batch size
 INIT_LR = 0.0001
-EPOCHS = 18
+EPOCHS = 20
 BS = 40
 
 # compile our model
 print("[INFO] compiling model...")
-opt = Adam(lr=INIT_LR, decay=INIT_LR / EPOCHS)
+opt = Adam(learning_rate=INIT_LR, decay=INIT_LR / EPOCHS)
 model.compile(loss="binary_crossentropy", optimizer=opt, metrics=["accuracy"])
 
 # train the head of the network
 print("[INFO] training head...")
-head = model.fit(
+H = model.fit(
 	augImg.flow(trainX, trainY, batch_size=BS),
 	steps_per_epoch=len(trainX) // BS,
 	validation_data=(testX, testY),
@@ -113,3 +115,17 @@ print(classification_report(testY.argmax(axis=1), pred,	target_names=lb.classes_
 # serialize the model to disk
 print("[INFO] saving mask detector model...")
 model.save("mask_detector.model", save_format="h5")
+
+# plot the training loss and accuracy
+N = EPOCHS
+plt.style.use("ggplot")
+plt.figure()
+plt.plot(np.arange(0, N), H.history["loss"], label="train_loss")
+plt.plot(np.arange(0, N), H.history["val_loss"], label="val_loss")
+plt.plot(np.arange(0, N), H.history["accuracy"], label="train_acc")
+plt.plot(np.arange(0, N), H.history["val_accuracy"], label="val_acc")
+plt.title("Training Loss and Accuracy")
+plt.xlabel("Epoch #")
+plt.ylabel("Loss/Accuracy")
+plt.legend(loc="lower left")
+plt.savefig("plot.png")
